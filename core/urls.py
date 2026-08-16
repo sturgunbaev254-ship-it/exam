af10121bbefe528d2ff3b9bb45e11268dd689ee4
sturgunbaev254-ship-it <sturgunbaev254@gmail.com>
2026-08-16 15:14:15 +0600

    first commit

--- core/urls.py
+++ core/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('class.urls')),
+    path('', include('exam.urls')),
 ]
 
 if settings.DEBUG:
